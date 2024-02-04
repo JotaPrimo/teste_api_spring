9a35ec58e7c26f5d72515c2df0c6bbd2eae32eeb
JotaPrimo <gestald118@gmail.com>
2024-02-04 11:32:17 -0400

    ApiExceptionHandler
    Adicionado exception para EntityNotFoundException

---
+++ src/main/java/com/testeapi/vagas/demo/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.testeapi.vagas.demo.web.exception;
+
+import com.testeapi.vagas.demo.exceptions.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException exception,
+                                                                        HttpServletRequest request,
+                                                                        BindingResult result) {
+        log.error("class ApiExceptionHandler - ", exception);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Erro de cadatro", "Campo(s) inválido(s)", result));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundException(EntityNotFoundException exception,
+                                                                HttpServletRequest request) {
+        log.error("class ApiExceptionHandler - ", exception);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, "Recurso não encontrado", exception.getMessage()));
+    }
+
+
+}
