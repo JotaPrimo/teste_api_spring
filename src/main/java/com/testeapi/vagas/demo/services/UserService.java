c854e9a0ca4c3dbd320c72b2d869d39a42fda9fa
Jota Santos <jailsonsantosdpge@outlook.com>
2024-02-15 15:37:45 -0400

    Service
    - Novos métodos para CRUD

--- src/main/java/com/testeapi/vagas/demo/services/UserService.java
+++ src/main/java/com/testeapi/vagas/demo/services/UserService.java
@@ -1,8 +1,8 @@
 package com.testeapi.vagas.demo.services;
 
 import com.testeapi.vagas.demo.entities.User;
+import com.testeapi.vagas.demo.exceptions.EntityNotFoundException;
 import com.testeapi.vagas.demo.repositories.UserRepository;
-import com.testeapi.vagas.demo.utils.TableFieldChecker;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -41,9 +41,41 @@ public class UserService {
         return userRepository.findAll(sort);
     }
 
+    @Transactional(readOnly = false)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Usuário de id #%s não foi encontrado", id))
+        );
+    }
+
     @Transactional
     public User store(User user) {
         return userRepository.save(user);
     }
 
+    @Transactional
+    public void delete(Long id) {
+        User user = this.findById(id);
+        userRepository.delete(user);
+    }
+
+    @Transactional
+    public User update(Long id, User userUpdate) {
+        User user = this.findById(id);
+
+        user.setCpf(userUpdate.getCpf());
+        user.setName(userUpdate.getName());
+        user.setEmail(userUpdate.getEmail());
+
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public User inativar(Long id) {
+        User user = this.findById(id);
+
+        user.setAtivo(User.INATIVO);
+
+       return userRepository.save(user);
+    }
 }
