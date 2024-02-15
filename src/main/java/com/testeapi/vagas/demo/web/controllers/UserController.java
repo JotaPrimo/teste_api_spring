0e6b1b68a13b0c44349bd0f2d07693cb2ee08fec
Jota Santos <jailsonsantosdpge@outlook.com>
2024-02-15 15:37:55 -0400

    Controller
    - Novos métodos para CRUD

--- src/main/java/com/testeapi/vagas/demo/web/controllers/UserController.java
+++ src/main/java/com/testeapi/vagas/demo/web/controllers/UserController.java
@@ -29,10 +29,39 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> show(@PathVariable Long id) {
+        User user = this.service.findById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
     @PostMapping
     public ResponseEntity<User> store(@Valid @RequestBody User user) {
         User userCreated = service.store(user);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> inativar(@PathVariable Long id) {
+        User user = service.inativar(id);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @Valid @RequestBody User userUpdate) {
+       User user =  this.service.update(id, userUpdate);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
