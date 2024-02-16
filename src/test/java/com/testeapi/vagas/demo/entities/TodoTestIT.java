c70b7d3d728344d41d62d15f17e5b851d1afff84
Jota Santos <jailsonsantosdpge@outlook.com>
2024-02-16 16:40:07 -0400

    Teste de Integração
    - Corrigido imports

--- src/test/java/com/testeapi/vagas/demo/entities/TodoTestIT.java
+++ src/test/java/com/testeapi/vagas/demo/entities/TodoTestIT.java
@@ -2,18 +2,16 @@ package com.testeapi.vagas.demo.entities;
 
 import com.testeapi.vagas.demo.enums.Prioridade;
 import com.testeapi.vagas.demo.path.ApiPaths;
-import com.testeapi.vagas.demo.web.controllers.TodoController;
 import com.testeapi.vagas.demo.web.dtos.TodoCreateDTO;
 import com.testeapi.vagas.demo.web.dtos.TodoResponseDTO;
 import com.testeapi.vagas.demo.web.exception.ErrorMessage;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // tomcant do teste ser? executado em porta randomica
