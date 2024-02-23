887c28931ff756714db2bc4b610baa2631e73fe2
Jota Santos <jailsonsantosdpge@outlook.com>
2024-02-23 16:50:34 -0400

    DTO
     - Adicionando user_id

--- src/main/java/com/testeapi/vagas/demo/web/dtos/TodoCreateDTO.java
+++ src/main/java/com/testeapi/vagas/demo/web/dtos/TodoCreateDTO.java
@@ -1,10 +1,12 @@
 package com.testeapi.vagas.demo.web.dtos;
 
+import com.testeapi.vagas.demo.entities.User;
 import com.testeapi.vagas.demo.enums.Prioridade;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class TodoCreateDTO {
@@ -21,13 +23,19 @@ public class TodoCreateDTO {
     @Enumerated(EnumType.STRING)
     private Prioridade prioridade;
 
+    @NotNull(message = "Informe o user id")
+    private Long user_id;
+
+    private User user;
+
     public TodoCreateDTO() {
     }
 
-    public TodoCreateDTO(String nome, String descricao, Prioridade prioridade) {
+    public TodoCreateDTO(String nome, String descricao, Prioridade prioridade, Long user_id) {
         this.nome = nome;
         this.descricao = descricao;
         this.prioridade = prioridade;
+        this.user_id = user_id;
     }
 
     public String getNome() {
@@ -54,6 +62,21 @@ public class TodoCreateDTO {
         this.prioridade = prioridade;
     }
 
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     @Override
     public String toString() {
